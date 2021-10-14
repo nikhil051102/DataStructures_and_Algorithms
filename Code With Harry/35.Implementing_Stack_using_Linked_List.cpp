9c96b40c87d59aae0580d5684be09f283042d597
nikhil051102 <nikhildeore05@gmail.com>
2021-10-14 18:25:40 +0530

    -

--- Code With Harry/35.Implementing_Stack_using_Linked_List.cpp
+++ Code With Harry/35.Implementing_Stack_using_Linked_List.cpp
@@ -1,59 +1,198 @@
 #include <iostream>
 using namespace std;
 
-struct Node 
+//Insertion and deletion of node will be done in 0(1) at Head Node that's why we prefer Head Node for operations.
+//Head will be used for push() and pop() operations.
+//This stack will net overflow bcz of Linked List we can add as many elements as we want.
+//When the head node is pointing towards NULL then this stack is Underflow.
+//Head is Top and Tail(Last Node) is Bottom.
+//Stack will overflow when Heap memory is Exhausted.
+
+struct Stack
 {
-    int data;
-    struct Node* address;
+    int size;
+    int top;
+    struct Node;
 };
 
-void display(struct Node *ptr)
+struct Node
 {
-    while(ptr!=NULL)
+    int data;
+    struct Node *address;
+};
+
+void LinkedListTraversal(struct Node *ptr)
+{
+    while (ptr != NULL)
     {
-        cout<<ptr->data<<endl;
-        ptr=ptr->address;
+        cout << "Element is " << ptr->data << endl;
+        ptr = ptr->address;
     }
 }
 
-struct Node *push(struct Node *top,int value)
+int isEmpty(struct Node *element)
 {
-    struct Node*First = (struct Node *)malloc(sizeof(struct Node));
-    First->data=value;
+    if (element == NULL)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+};
 
-    First->address=top;
-    top=First;
-    return top;
+int isFull(struct Node *ptr)
+{
+    struct Node *n = (struct Node *)malloc(sizeof(struct Node));
+    if (n == NULL)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
 }
 
-int pop(struct Node *top)
+struct Node *push(struct Node *top, int value)
 {
-    struct Node* last=top;
-    top=top->address;
-    int element=last->data;
-    free(last);
-    return element;
-
+    if (isFull(top))
+    {
+        cout << "Stack Overflow." << endl;
+    }
+    else
+    {
+        struct Node *n = (struct Node *)malloc(sizeof(struct Node));
+        n->data = value;
+        n->address = top;
+        top = n;
+        return top;
+    }
 }
 
-
-
+int pop(struct Node **top)
+{
+    if (isEmpty(*top))
+    {
+        cout << "Stack Underflow" << endl;
+    }
+    else
+    {
+        struct Node *n = *top;
+        *top = (*top)->address;
+        int x = n->data;
+        free(n);
+        return x;
+    }
+}
 
 int main()
 {
-    
-    
 
-    struct Node *top=NULL;
-    top=push(top,344);
-    top=push(top,766);
-    top=push(top,123);
-    display(top);
-    int value=pop(top);
-    cout<<"the poped value is"<<value;
+    struct Node *top = NULL;
+    top = push(top, 1000);
+    top = push(top, 250);
+    top = push(top, 3000);
+    top = push(top, 2000);
 
-
-
-
+    int element = pop(&top);
+    cout << "Popped Element is " << element << endl;
+    LinkedListTraversal(top);
     return 0;
 }
+
+// //Method 2
+// struct Node *top = NULL;
+
+// struct Stack
+// {
+//     int size;
+//     int top;
+//     struct Node;
+// };
+
+// struct Node
+// {
+//     int data;
+//     struct Node *address;
+// };
+
+// void LinkedListTraversal(struct Node *ptr)
+// {
+//     while (ptr != NULL)
+//     {
+//         cout << "Element is " << ptr->data << endl;
+//         ptr = ptr->address;
+//     }
+// }
+
+// int isEmpty(struct Node *element)
+// {
+//     if (element == NULL)
+//     {
+//         return 1;
+//     }
+//     else
+//     {
+//         return 0;
+//     }
+// };
+
+// int isFull(struct Node *ptr)
+// {
+//     struct Node *n = (struct Node *)malloc(sizeof(struct Node));
+//     if (n == NULL)
+//     {
+//         return 1;
+//     }
+//     else
+//     {
+//         return 0;
+//     }
+// }
+
+// struct Node *push(struct Node *top, int value)
+// {
+//     if (isFull(top))
+//     {
+//         cout << "Stack Overflow." << endl;
+//     }
+//     else
+//     {
+//         struct Node *n = (struct Node *)malloc(sizeof(struct Node));
+//         n->data = value;
+//         n->address = top;
+//         top = n;
+//         return top;
+//     }
+// }
+
+// int pop(struct Node *element)
+// {
+//     if (isEmpty(element))
+//     {
+//         cout << "Stack Underflow" << endl;
+//     }
+//     else
+//     {
+//         struct Node *n = element;
+//         top = element->address;
+//         int x = n->data;
+//         free(n);
+//         return x;
+//     }
+// }
+
+// int main()
+// {
+//     top = push(top, 1000);
+//     top = push(top, 250);
+//     top = push(top, 3000);
+//     top = push(top, 2000);
+
+//     int element = pop(&top);
+//     cout << "Popped Element is " << element << endl;
+//     LinkedListTraversal(top);
+//     return 0;
+// }
