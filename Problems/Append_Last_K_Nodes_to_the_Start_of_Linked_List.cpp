5e25c535f3af0bd736620e7427a7bd092a152192
nikhil051102 <nikhildeore05@gmail.com>
2021-10-16 13:11:00 +0530

    -

--- Problems/Append_Last_K_Nodes_to_the_Start_of_Linked_List.cpp
+++ Problems/Append_Last_K_Nodes_to_the_Start_of_Linked_List.cpp
@@ -23,19 +23,51 @@ int LinkedListTraversal(struct Node *ptr)
     while (ptr != NULL)
     {
         count++;
+        cout<<"Element : "<<ptr->data<<endl;
         ptr = ptr->next;
     }
     return count;
 }
 
+//We will break first (Number of Nodes - k) nodes from linked list by pointing last node to NULL
+struct Node* Breaking_Nodes(struct Node*ptr, int x)
+{
+    for (int i = 0; i < x-1; i++)
+    {
+        ptr = ptr->next;
+    }
+    struct Node*n = ptr->next;
+    ptr->next = NULL;
+    return n;
+}
+
+void Appending_Node(struct Node*ptr1, struct Node*ptr2, int k)
+{
+    for (int i = 0; i < k-1; i++)
+    {
+        ptr2 = ptr2->next;
+    }
+    ptr2->next = ptr1;
+    // struct Node*n = ptr2;
+    // return n;
+}
 
 int main()
 {
+    int k = 2;
     struct Node*s;
     s = Insert(s, 1000);
     s = Insert(s, 2000);
     s = Insert(s, 3000);
     s = Insert(s, 4000);
     s = Insert(s, 5000);
+
+    int total_nodes = (LinkedListTraversal(s));
+    cout<<endl<<endl<<endl;
+    struct Node*n = Breaking_Nodes(s, total_nodes-k);
+    cout<<LinkedListTraversal(s)<<endl;
+    cout<<LinkedListTraversal(n)<<endl;
+    Appending_Node(s, n, k);
+    cout<<LinkedListTraversal(n);
     return 0;
 }
