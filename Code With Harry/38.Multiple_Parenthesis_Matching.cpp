f8764d39a34b2079a0ea3c5e2f46f5e2e61ca99f
nikhil051102 <nikhildeore05@gmail.com>
2021-10-19 16:26:03 +0530

    -

--- Code With Harry/38.Multiple_Parenthesis_Matching.cpp
+++ Code With Harry/38.Multiple_Parenthesis_Matching.cpp
@@ -8,30 +8,71 @@ struct Stack
     char *array;
 };
 
+int isEmpty(struct Stack *ptr)
+{
+    if (ptr->top == -1)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+void push(struct Stack *ptr, char value)
+{
+    ptr->top++;
+    ptr->array[ptr->top] = value;
+}
+
+void pop(struct Stack *ptr)
+{
+    ptr->top--;
+}
+
 
 int main()
 {
     int n;
-    cout << "Number of Characters : ";
+    cout << "Number of Elements to be Stored : ";
     cin >> n;
-
-    char array[n];
-    cout<<"Enter Characters : "<<endl;
-    for (int i = 0; i < n; i++)
-    {
-        cin>>array[i];
-    }
-    
 
     struct Stack *s = (struct Stack *)malloc(sizeof(struct Stack));
     s->size = n;
     s->top = -1;
-    s->array = (char *)malloc(s->size*sizeof(char));
+    s->array = (char *)malloc(s->size * sizeof(char));
 
-    // for (int j = 0; j < n; j++)
-    // {
-    // }
-    
-    
+    cout << "Enter Characters : ";
+    char array[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> array[i];
+    }
+
+    for (int j = 0; j < n; j++)
+    {
+        if (array[j] == '(')
+        {
+            push(s, array[j]);
+        }
+        else if (array[j] == ')')
+        {
+            pop(s);
+        }
+    }
+
+    if (isEmpty(s))
+    {
+        cout<<"Operation Successful"<<endl;
+    }
+    else if (s->top <-1)
+    {
+        cout<<"Stack Underflow by "<<(s->top - (-1))<<endl;
+    }
+    else if (s->top > -1)
+    {
+        cout<<"Stack Overflow by "<<(s->top -(-1))<<endl;
+    }
     return 0;
 }
