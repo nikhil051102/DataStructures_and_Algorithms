872fade2241211560ee9162eb7265a723bebffb3
nikhil051102 <nikhildeore05@gmail.com>
2021-10-20 13:39:14 +0530

    -

--- Put_Even_Position_Nodes_After_Odd.cpp
+++ Put_Even_Position_Nodes_After_Odd.cpp
@@ -4,53 +4,55 @@ using namespace std;
 struct Node
 {
     int data;
-    struct Node*address;
+    struct Node *address;
 };
 
-struct Node* Insert(struct Node*head, struct Node*ptr, int value)
+struct Node *Insert(struct Node *head, struct Node *ptr, int value)
 {
-    struct Node*n = (struct Node*)malloc(sizeof(struct Node));
+    struct Node *n = (struct Node *)malloc(sizeof(struct Node));
     n->data = value;
     n->address = ptr->address;
     ptr->address = n;
     return head;
 }
 
-int LinkedListTraversal(struct Node*ptr)
+int LinkedListTraversal(struct Node *ptr)
 {
-    int count = 0;
     while (ptr != NULL)
     {
-        cout<<"Element : "<<ptr->data<<endl;
+        cout << "Element : " << ptr->data << endl;
         ptr = ptr->address;
-        count++;
     }
-    return count;
 }
 
-struct Node*Answer(struct Node*ptr)
+void Answer(struct Node *ptr)
 {
-    struct Node*odd = ptr;
-    struct Node*even = ptr->address;
+    struct Node *odd = ptr;
+    struct Node *even = ptr->address;
+    struct Node *evenStart = even;
 
-    while (odd != NULL or even != NULL)
+    while (odd->address != NULL && even->address != NULL)
     {
         odd->address = even->address;
+        odd = odd->address;
         even->address = odd->address;
+        even = even->address;
     }
-    odd->address = even;
-
-    return odd;
+    odd->address = evenStart;
+    if (odd->address != NULL)
+    {
+        even->address = NULL;
+    }
 }
 
 int main()
 {
-    struct Node*head = (struct Node*)malloc(sizeof(struct Node));
-    struct Node*second = (struct Node*)malloc(sizeof(struct Node));
-    struct Node*third = (struct Node*)malloc(sizeof(struct Node));
-    struct Node*fourth = (struct Node*)malloc(sizeof(struct Node));
-    struct Node*fifth = (struct Node*)malloc(sizeof(struct Node));
-    struct Node*sixth = (struct Node*)malloc(sizeof(struct Node));
+    struct Node *head = (struct Node *)malloc(sizeof(struct Node));
+    struct Node *second = (struct Node *)malloc(sizeof(struct Node));
+    struct Node *third = (struct Node *)malloc(sizeof(struct Node));
+    struct Node *fourth = (struct Node *)malloc(sizeof(struct Node));
+    struct Node *fifth = (struct Node *)malloc(sizeof(struct Node));
+    struct Node *sixth = (struct Node *)malloc(sizeof(struct Node));
 
     head->data = 1;
     head->address = second;
@@ -70,9 +72,9 @@ int main()
     sixth->data = 6;
     sixth->address = NULL;
 
-    int n = LinkedListTraversal(head);
-    struct Node*newNode = Answer(head);
-
-    LinkedListTraversal(newNode);
+    LinkedListTraversal(head);
+    Answer(head);
+    cout << endl;
+    LinkedListTraversal(head);
     return 0;
 }
