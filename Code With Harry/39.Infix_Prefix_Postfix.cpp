ad20fb5b3e947e2e495acc107e589944b2865c2c
nikhil051102 <nikhildeore05@gmail.com>
2021-10-21 09:16:58 +0530

    -

---
+++ Code With Harry/39.Infix_Prefix_Postfix.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    //Notations to write an Expressions.
+    //1) Infix : operand <operator> operand
+    //          a)a+b b)p/q c)a-b d)x-4
+    //2) Prefix : <operator> operand1 operand2
+    //          +ab, -pq, /xy
+    //3) Postfix : operand1 operand2 <operator>
+    //          ab+, pq-, xy/
+    return 0;
+}
