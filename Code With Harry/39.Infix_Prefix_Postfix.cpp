e7e1519bf0a5a809719f96155a00e6c8b5fb10a2
nikhil051102 <nikhildeore05@gmail.com>
2021-10-23 12:46:47 +0530

    -

--- Code With Harry/39.Infix_Prefix_Postfix.cpp
+++ Code With Harry/39.Infix_Prefix_Postfix.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <string>
 using namespace std;
 
 struct Stack
@@ -9,8 +8,105 @@ struct Stack
     char *array;
 };
 
-char * InfixToPostfix(char *infix, int n)
+int isEmpty(Stack*ptr)
 {
+    if (ptr->top == -1)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+int StackTop(Stack*ptr)
+{
+    return ptr->array[ptr->top];
+}
+
+int Precedence(char ch)
+{
+    if (ch == '*' || ch == '/')
+    {
+        return 2;
+    }
+    else if (ch == '+' || ch == '-')
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+int isOperator(char ch)
+{
+    if (ch == '+' || ch =='-' || ch == '*' || ch == '/')
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+void push(Stack*sp, char value)
+{
+    sp->top++;
+    sp->array[sp->top] = value;
+}
+
+char pop(Stack*sp)
+{
+    char val = sp->array[sp->top];
+    sp->top--;
+    return val;
+}
+
+char *InfixToPostfix(char *Infix, int n)
+{
+    struct Stack *sp;
+    sp->size = n;
+    sp->top = -1;
+    sp->array = (char *)malloc(sp->size*sizeof(char));
+    
+    char *Postfix = (char *)malloc((n+1)*sizeof(char));
+    int i = 0;
+    int j = 0;
+
+    while (i != n)
+    {
+        if(!isOperator(Infix[i]))
+        {
+            Postfix[j] = Infix[i];
+            i++;
+            j++;
+        }
+        else
+        {
+            if (Precedence(Infix[i]) < Precedence(StackTop(sp)))
+            {
+                push(sp, Infix[i]);
+                i++;
+            }
+            else
+            {
+                Postfix[i] = pop(sp);
+                j++;
+            }
+
+        }
+        while (!isEmpty(sp))
+        {
+            Postfix[j] = pop(sp);
+            j++;
+        }
+        
+    }
+    return Postfix;
 }
 
 int main()
@@ -24,12 +120,17 @@ int main()
     //          ab+, pq-, xy/
 
     int n;
-    char infix[n];
-    cout<<"Infix Expression : "<<endl;
+    cout<<"Number of Characters : ";
+    cin>>n;
+
+    char expression[n];
+    cout<<"Enter Characters : "<<endl;
     for (int i = 0; i < n; i++)
     {
-        cin>>infix[i];
+        cin>>expression[i];
     }
 
+    char *Postfix = InfixToPostfix(expression, n);
+    
     return 0;
 }
