de5f373d18dd4a1902f4433a461c23adaadcb2ea
nikhil051102 <nikhildeore05@gmail.com>
2021-10-31 15:57:07 +0530

    -

---
+++ Code With Harry/43.Double_Ended_Queue.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+//DEQueue doesn't follow FIFO Priciple
+//-> Insertion and Deletion from Front and End is Possible.
+
+int main()
+{
+    
+    return 0;
+}
