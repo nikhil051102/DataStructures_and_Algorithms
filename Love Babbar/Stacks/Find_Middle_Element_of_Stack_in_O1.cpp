64b60d87f52ff1d74d0baf1dc3770052d62fc221
nikhil051102 <nikhildeore05@gmail.com>
2021-12-05 22:47:41 +0530

    -

--- Love Babbar/Stacks/Find_Middle_Element_of_Stack_in_O1.cpp
+++ Love Babbar/Stacks/Find_Middle_Element_of_Stack_in_O1.cpp
@@ -2,53 +2,7 @@
 using namespace std;
 
 // Problem Link : https://www.geeksforgeeks.org/design-a-stack-with-find-middle-operation/
-
-struct Node
-{
-    struct Node*previous;
-    int data;
-    struct Node*next;
-};
-
-struct Node *Push(struct Node*Top, int value)
-{
-    struct Node*n = (struct Node*)malloc(sizeof(struct Node));
-    n->data = value;
-    n->previous = Top;
-    n->next = Top;
-    return n;
-}
-
-void ListTraversal(struct Node *ptr)
-{
-    while (ptr->next != NULL)
-    {
-        cout << "Element is " << ptr->data << endl;
-        ptr = ptr->next;
-    }
-}
-// int FindMiddle(struct Stack*s)
-// {
-//     int x;
-//     if ((s->top+1)%2 == 0)
-//     {
-//         x = (s->top)/2 + 1;
-//         return s->array[x];
-//     }
-//     else
-//     {
-//         x = (s->top)/2;
-//         return s->array[x];
-//     }
-// }
-
 int main()
 {
-    struct Node*Top = (struct Node*)malloc(sizeof(struct Node));
-    Top = Push(Top, 100);
-    Top = Push(Top, 200);
-    Top = Push(Top, 300);
-
-    ListTraversal(Top);
     return 0;
 }
