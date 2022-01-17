0d1d6b03bced8af5330ccabcc1ffa22ec8e3701e
nikhil051102 <nikhildeore05@gmail.com>
2022-01-17 15:19:55 +0530

    -

--- Algorithms/Selection Sort/main.cpp
+++ Algorithms/Selection Sort/main.cpp
@@ -4,7 +4,7 @@ using namespace std;
 void Selection_Sort(int *array, int n)
 {
     int temp;
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < n-1; i++)
     {
         for (int j = i+1; j < n; j++)
         {
